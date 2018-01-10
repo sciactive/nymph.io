e205542287fb1b80cf2c8270a62945afffc67816
Hunter Perrin <hperrin@gmail.com>
2018-01-10 13:20:10 -0800

    Added versioned API docs.

--- generate-docs.sh
+++ generate-docs.sh
@@ -2,4 +2,4 @@
 
 cd $(dirname $0)
 
-./vendor/apigen/apigen/bin/apigen generate -s ../server/ -d ./api/server --exclude vendor/ --exclude testing/ --exclude bower_components/ --exclude node_modules/ --title "Nymph Server API" --tree --main Nymph
+./vendor/apigen/apigen/bin/apigen generate -s ../server/ -d ./api/server/$1/ --exclude vendor/ --exclude testing/ --exclude bower_components/ --exclude node_modules/ --title "Nymph $1 Server API" --tree --main Nymph
