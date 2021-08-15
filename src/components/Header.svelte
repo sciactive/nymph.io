fc97b54fb5bf20c8340a42ce774229e5858cc95e
Hunter Perrin <hperrin@gmail.com>
2021-08-15 16:39:44 -0700

    Initial commit.

---
+++ src/components/Header.svelte
@@ -0,0 +1,12 @@
+<!-- Header -->
+<header id="header">
+  <div class="logo"><a href="index.html" class="logo"><strong>SciActive Site</strong></a> - <slot /></div>
+  <ul class="icons">
+    <li>
+      <a href="https://twitter.com/SciActive" target="_blank" class="icon brands fa-twitter"><span class="label">Twitter</span></a>
+    </li>
+    <li>
+      <a href="https://github.com/sciactive" target="_blank" class="icon brands fa-github"><span class="label">GitHub</span></a>
+    </li>
+  </ul>
+</header>
---
+++ src/routes/index.svelte
@@ -0,0 +1,38 @@
+<!-- Main -->
+<div id="main">
+  <div class="inner">
+    <Header>Index Page</Header>
+
+    <!-- Banner -->
+    <section id="banner">
+      <div class="content">
+        <header>
+          <h1>
+            SciActive Site
+          </h1>
+          <p>A site for a SciActive project.</p>
+        </header>
+        <p>
+          The description of the SciActive project. This whole paragraph is full of descriptive text regarding the project. The project is described, in detail, by all the text found within this paragraph.
+        </p>
+        <ul class="actions">
+          <li><a href="#" class="button big">Learn More</a></li>
+        </ul>
+      </div>
+    </section>
+
+    <!-- Section -->
+    <section>
+      <header class="major">
+        <h2>Here's the site!</h2>
+      </header>
+      <div>
+        Check out this awesome site. It's a site to behold.
+      </div>
+    </section>
+  </div>
+</div>
+
+<script lang="ts">
+  import Header from '../components/Header.svelte';
+</script>
