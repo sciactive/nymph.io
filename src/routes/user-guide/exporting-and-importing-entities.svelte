c0e5b950ddaf07c0a49e1c09ae72bb562409c8f3
Hunter Perrin <hperrin@gmail.com>
2021-09-06 08:24:47 -0700

    Migrated Exporting and Importing Entities and Subscribing to Queries.

--- src/routes/user-guide/exporting-and-importing-entities.svelte
+++ src/routes/user-guide/exporting-and-importing-entities.svelte
@@ -1,5 +1,6 @@
 <svelte:head>
   <title>Exporting and Importing Entities - User Guide - Nymph</title>
+  {@html github}
 </svelte:head>
 
 <section>
@@ -7,14 +8,72 @@
     <h1 style="font-size: 3em;">Exporting and Importing Entities</h1>
   </header>
 
-  <p>section</p>
+  <p>
+    Nymph provides a method for exporting and importing entities, regardless of
+    which driver is currently in use. This allows backup, restore, duplication,
+    or transferring an entire database of entities, even with different database
+    drivers.
+  </p>
 
-  <header class="major">
-    <h2>sub</h2>
-  </header>
+  <p>Nymph has the following methods for exporting and importing entities.</p>
 
-  <p>section</p>
+  <ul>
+    <li><code>export</code> - Export entities to a file.</li>
+    <li>
+      <code>exportPrint</code> - Export entities to the client as a downloadable
+      file.
+    </li>
+    <li><code>import</code> - Import entities from a file.</li>
+  </ul>
+
+  <Highlight
+    language={typescript}
+    code={`// Export entities to a file.
+Nymph.export(filename);
+
+// Export entities to the console.
+Nymph.exportPrint();
+
+// Import entities from a file.
+Nymph.import(filename);`}
+  />
+
+  <p>
+    The format of an entity export file is simple, and readable by all drivers.
+    The file extension, nex, stands for Nymph Entity eXchange.
+  </p>
+
+  <Highlight
+    code={`#nex2
+# The above line must be the first thing in the file.
+# Comments begin with #
+   # And can have white space before them.
+# This defines a UID.
+<name/of/uid>[5]
+<another uid>[8000]
+# For UIDs, the name is in angle brackets (<>) and the value follows
+# in square brackets ([]).
+# This starts a new entity.
+{1234abcd}<etype>[tag,list,with,commas]
+# For entities, the GUID is in curly brackets ({}), then the etype in
+#  angle brackets, then the comma separated tag list follows in square
+#  brackets ([]).
+# Properties are stored like this:
+# propname=JSON.stringify(value)
+    abilities=["system/all"]
+    groups=[]
+    inheritAbilities=false
+    name="admin"
+# White space before/after "=" and at beginning/end of line is ignored.
+        username  =     "admin"
+{2}<etype>[tag,list]
+    another="This is another entity."
+    newline="\\n"`}
+  />
 </section>
 
 <script lang="ts">
+  import Highlight from 'svelte-highlight';
+  import typescript from 'svelte-highlight/src/languages/typescript';
+  import github from 'svelte-highlight/src/styles/github';
 </script>
--- src/routes/user-guide/subscribing-to-queries.svelte
+++ src/routes/user-guide/subscribing-to-queries.svelte
@@ -1,5 +1,6 @@
 <svelte:head>
   <title>Subscribing to Queries - User Guide - Nymph</title>
+  {@html github}
 </svelte:head>
 
 <section>
@@ -7,14 +8,163 @@
     <h1 style="font-size: 3em;">Subscribing to Queries</h1>
   </header>
 
-  <p>section</p>
+  <p>
+    Subscribing to a query lets you receive updates to that query. This is a
+    powerful tool for collaborative apps. It's also just as easy as making a
+    regular entity query.
+  </p>
 
-  <header class="major">
-    <h2>sub</h2>
-  </header>
+  <p>
+    Before we get started, you can follow the <a href="{base}/packages/pubsub/"
+      >PubSub Server Package</a
+    > page to set up a Nymph PubSub server.
+  </p>
 
-  <p>section</p>
+  <p>
+    You can use the <code>subscribeEntities</code> method on PubSub to subscribe
+    to a query. It will return a function you can call to subscribe to the
+    query. That function accepts a resolve and reject callback, similar to a
+    promise. The resolve function will receive an update, which will at first be
+    an array of entities, then will be an update object with instructions that
+    <code>updateArray</code> uses to update the original array.
+  </p>
+
+  <Highlight
+    language={typescript}
+    code={`let smiths: (User & UserData)[] = [];
+PubSub.subscribeEntities(
+  {
+    class: User
+  },
+  {
+    type: '&',
+    like: ['name', '% Smith']
+  }
+)(
+  (update) => {
+    // This function will be called once initially with an array of
+    // entities, then again every time there's a change with an
+    // update object.
+    // The updateArray function will add any newly matching entities,
+    // update any existing entities that have changed, and remove any
+    // entities that no longer match (including deleted entities).
+    PubSub.updateArray(smiths, update);
+  },
+  (e) => {
+    alert('Error: ' + e.textStatus);
+  }
+);`}
+  />
+
+  <p>
+    You can also receive a count of how many subscribers there are to that
+    query. The count is given to a third callback.
+  </p>
+
+  <Highlight
+    language={typescript}
+    code={`let smiths: (User & UserData)[] = [];
+PubSub.subscribeEntities(
+  {
+    class: User
+  },
+  {
+    type: '&',
+    like: ['name', '% Smith']
+  }
+)(
+  (update) => {
+    PubSub.updateArray(smiths, update);
+  },
+  (e) => {
+    alert('Error: ' + e.textStatus);
+  },
+  (count) => {
+    // This function will be called each time the count of
+    // subscribers to this query changes.
+    console.log(\`There are now \${count} users watching the Smiths.\`);
+  }
+);`}
+  />
+
+  <p>
+    To unsubscribe from the query, use the <code>unsubscribe</code> method on
+    the
+    <code>PubSubSubscription</code> object returned by the function.
+  </p>
+
+  <Highlight
+    language={typescript}
+    code={`let smiths: (User & UserData)[] = [];
+let subscription = PubSub.subscribeEntities(
+  {
+    class: User
+  },
+  {
+    type: '&',
+    like: ['name', '% Smith']
+  }
+)((update) => {
+  PubSub.updateArray(smiths, update);
+});
+
+let unsubscribe = () => {
+  subscription.unsubscribe();
+};`}
+  />
+
+  <p>
+    You can subscribe to changes to an entity with the <code>subscribeWith</code
+    >
+    method. This method accepts the callbacks directly and returns a
+    <code>PubSubSubscription</code>
+    object.
+  </p>
+
+  <Highlight
+    language={typescript}
+    code={`let subscription = PubSub.subscribeWith(
+  smithEntity,
+  () => {
+    if (smithEntity.guid == null) {
+      if (confirm('Someone deleted Mr. Smith! Do you want to restore him?')) {
+        smithEntity.$save();
+      }
+    } else {
+      alert("Mr. Smith's entity has changed!");
+    }
+  },
+  (e) => {
+    alert('Error: ' + e.textStatus);
+  },
+  (count) => {
+    console.log(\`There are now \${count} users watching Mr. Smith.\`);
+  }
+);
+
+let unsubscribe = () => {
+  subscription.unsubscribe();
+};`}
+  />
+
+  <p>
+    Warning: Subscriptions can lead to resource leaks if left open. Take care to
+    unsubscribe to every query you are no longer watching.
+  </p>
+
+  <p>
+    Warning: The PubSub server is smart about removing entities that no longer
+    match when a new entity matches, but there are some queries that can have
+    changes in their matching entities without any change to the server (like
+    relative time queries). These queries won't necessarily be updated correctly
+    in due time by the PubSub server.
+  </p>
 </section>
 
 <script lang="ts">
+  import Highlight from 'svelte-highlight';
+  import typescript from 'svelte-highlight/src/languages/typescript';
+  import github from 'svelte-highlight/src/styles/github';
+
+  import { base } from '$app/paths';
 </script>
